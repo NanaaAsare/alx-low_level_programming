0ddfec21a966baaafb76e1319b9d461c2ceaa9a8
NanaaAsare <nananyarkoasare74@gmail.com>
2023-02-17 15:36:11 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#incleude <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
