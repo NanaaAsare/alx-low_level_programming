35030c5ec99b1e4815067ad2f2dd5d6fbece3065
NanaaAsare <nananyarkoasare74@gmail.com>
2023-03-08 06:52:03 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - contar caracteres
+ * @s: string
+ * Return: number of chars
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s != '\0')
+	{
+		n = _strlen_recursion(s + 1);
+		n = n + 1;
+	}
+	return (n);
+}
