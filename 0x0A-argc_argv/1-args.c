b2eb4ceb3fc4ee5b863b00bd4efd64dc3a03212c
NanaaAsare <nananyarkoasare74@gmail.com>
2023-03-10 06:19:07 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - first argument
+ * @argc: unused
+ * @argv: string
+ * Return: string
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
