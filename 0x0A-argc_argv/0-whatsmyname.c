6e7f2c814b94fdb3c65afeff8dea5bc3be7d4e45
NanaaAsare <nananyarkoasare74@gmail.com>
2023-03-10 06:16:34 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - first argument
+ * @argc: unused
+ * @argv: string
+ * Return: string
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
