f0b52d0983ea811dd671575b73cf4f1f42fcd227
NanaaAsare <nananyarkoasare74@gmail.com>
2023-03-10 15:04:55 -0800

    0x09-static_libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string.
+ * @s: string
+ * @c: character to find
+ * Return: pointer to character to find or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+		return (s);
+		}
+	}
+	if (c  == '\0')
+	{
+		return (s);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - computer absolute value
+ * @a: num
+ * Return: result
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+
+}
+
