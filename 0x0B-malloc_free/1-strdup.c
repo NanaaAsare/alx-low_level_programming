a0690e54a7602cd29d1da3ce30c52d079ac0cf15
NanaaAsare <nananyarkoasare74@gmail.com>
2023-03-14 02:48:11 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _len - serch of size of a  string
+ * @string: stringn.
+ * Return: size of string.
+ */
+int _len(char *string)
+{
+	int len;
+
+	for (len = 0; string[len] != '\0'; len++)
+	;
+	return (len);
+}
+/**
+ * _strdup - contains a copy of the string given as a parameter.
+ * @str: string to  copy
+ * Return: pointer to new string.
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = _len(str);
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
