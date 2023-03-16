fddcc2cf6700bc390b30296ce53534d8ac62ef28
NanaaAsare <nananyarkoasare74@gmail.com>
2023-03-16 10:27:39 -0700

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - check
+ * @b: integer to malloc
+ * Return: 0
+ **/
+void *malloc_checked(unsigned int b)
+{
+	void *v;
+
+	v = malloc(b);
+
+	if (v == NULL)
+	{
+		exit(98);
+	}
+	return (v);
+}
