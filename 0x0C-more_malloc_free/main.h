602685160c1a3153a73881ecc40e2a5ca036d7e1
NanaaAsare <nananyarkoasare74@gmail.com>
2023-03-17 03:46:16 -0700

    funtion prototype

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -14,6 +14,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int *array_range(int min, int max);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
